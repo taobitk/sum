e1df9c10b34c297323fbe2de1a41abe39e52b1d7
taobitk <8hungvip8@gmail.com>
2024-08-14 19:08:59 +0700

    lần 1

---
+++ a.js
@@ -0,0 +1,6 @@
+function a(){
+    let nub1 = parseInt(document.getElementById('nub1').value);
+    let nub2 = parseInt(document.getElementById('nub2').value);
+    let nub3 = nub1 + nub2;
+    document.getElementById('nub3').innerHTML = nub3;
+}
