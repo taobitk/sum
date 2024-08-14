ac7e5cfa9e9e44999f080076ec1386f63578d826
taobitk <8hungvip8@gmail.com>
2024-08-14 19:45:06 +0700

    lần 3

--- a.js
+++ a.js
@@ -1,6 +1,20 @@
 function a(){
     let nub1 = parseInt(document.getElementById('nub1').value);
     let nub2 = parseInt(document.getElementById('nub2').value);
-    let nub3 = nub1 + nub2;
-    document.getElementById('nub3').innerHTML = nub3;
+    document.getElementById('nub3').innerHTML = "kq"+ (nub1 + nub2);
+}
+function b(){
+    let nub1 = parseInt(document.getElementById('nub1').value);
+    let nub2 = parseInt(document.getElementById('nub2').value);
+    document.getElementById('nub3').innerHTML = "kq"+ (nub1 - nub2);
+}
+function c(){
+    let nub1 = parseInt(document.getElementById('nub1').value);
+    let nub2 = parseInt(document.getElementById('nub2').value);
+    document.getElementById('nub3').innerHTML = "kq"+ (nub1 * nub2);
+}
+function d(){
+    let nub1 = parseInt(document.getElementById('nub1').value);
+    let nub2 = parseInt(document.getElementById('nub2').value);
+    document.getElementById('nub3').innerHTML = "kq"+ (nub1 / nub2);
 }
