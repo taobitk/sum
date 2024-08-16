5f39d858054f6067d26fffe8d365b74d033b83d1
taobitk <8hungvip8@gmail.com>
2024-08-16 18:59:59 +0700

    pt1

---
+++ phương trình bậc 1/pt1.js
@@ -0,0 +1,5 @@
+function pt1(){
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+   document.getElementById("kq").innerHTML = 'kết quả = '+ -b/a;
+}
