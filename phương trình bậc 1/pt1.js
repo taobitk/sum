5b2ca291b13499c156e46a372e2a201f5b64f041
taobitk <8hungvip8@gmail.com>
2024-08-16 19:17:56 +0700

    nokotan

--- phương trình bậc 1/pt1.js
+++ phương trình bậc 1/pt1.js
@@ -1,8 +1,13 @@
 function pt1(){
-    let a = document.getElementById("a").value;
-    let b = document.getElementById("b").value;
-    if (a == 0){
-        document.getElementById("kq").innerHTML = 'phương trình vô nghiệm';
+    let a = parseInt(document.getElementById("a").value) ;
+    let b = parseInt( document.getElementById("b").value);
+    
+    if (a === 0){
+        if (b === 0){
+            document.getElementById("kq").innerHTML = 'phương trình vô số nghiệm';
+        }else {
+            document.getElementById("kq").innerHTML = 'phương trình vô nghiệm';
+        }
     }else {
         document.getElementById("kq").innerHTML = 'kết quả = '+ -b/a;
     }
